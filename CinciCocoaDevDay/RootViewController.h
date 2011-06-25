054731f915fcfa93b35e45232cb402a42538aeb9
Peter Kananen <pkananen@gmail.com>
2011-06-25 12:23:55 -0400

    initial project setup with cocos2d and box2d

---
+++ CinciCocoaDevDay/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  CinciCocoaDevDay
+//
+//  Created by Peter Kananen on 6/25/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
