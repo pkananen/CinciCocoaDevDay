cafc37ff0701ecddf51b0ebf065cb180c2860f4b
David Miller <davidmiller1@gmail.com>
2011-06-25 14:21:54 -0400

    initial background

---
+++ CinciCocoaDevDay/background.h
@@ -0,0 +1,17 @@
+//
+//  background.h
+//  CinciCocoaDevDay
+//
+//  Created by David Miller on 6/25/11.
+//  Copyright 2011 NHXHN, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+
+@interface background : CCNode {
+    
+}
+
+@end
---
+++ CinciCocoaDevDay/background.m
@@ -0,0 +1,14 @@
+//
+//  background.m
+//  CinciCocoaDevDay
+//
+//  Created by David Miller on 6/25/11.
+//  Copyright 2011 NHXHN, LLC. All rights reserved.
+//
+
+#import "background.h"
+
+
+@implementation background
+
+@end
